b7a2dce5f52dfea5f5818c98c65424d7ec3f2b13
NikitaBabenko <33519548+NikitaBabenko@users.noreply.github.com>
2026-03-26 10:20:00 +0300

    V9_2_hyena_t

--- train_gpt.py
+++ train_gpt.py
@@ -75,8 +75,8 @@ class Hyperparameters:
     head_lr = float(os.environ.get("HEAD_LR", 0.002))
     tied_embed_lr = float(os.environ.get("TIED_EMBED_LR", 0.002))
     tied_embed_init_std = float(os.environ.get("TIED_EMBED_INIT_STD", 0.005))
-    matrix_lr = float(os.environ.get("MATRIX_LR", 0.0005))
-    scalar_lr = float(os.environ.get("SCALAR_LR", 0.0005))
+    matrix_lr = float(os.environ.get("MATRIX_LR", 0.0002))
+    scalar_lr = float(os.environ.get("SCALAR_LR", 0.0002))
     muon_momentum = float(os.environ.get("MUON_MOMENTUM", 0.95))
     muon_backend_steps = int(os.environ.get("MUON_BACKEND_STEPS", 5))
     muon_momentum_warmup_start = float(os.environ.get("MUON_MOMENTUM_WARMUP_START", 0.85))
@@ -84,7 +84,7 @@ class Hyperparameters:
     beta1 = float(os.environ.get("BETA1", 0.9))
     beta2 = float(os.environ.get("BETA2", 0.95))
     adam_eps = float(os.environ.get("ADAM_EPS", 1e-8))
-    grad_clip_norm = float(os.environ.get("GRAD_CLIP_NORM", 1.0))
+    grad_clip_norm = float(os.environ.get("GRAD_CLIP_NORM", 0.5))
 
     # Test-time training (LoRA) hyperparameters.
     ttt_lora_rank = int(os.environ.get("TTT_LORA_RANK", 8))
